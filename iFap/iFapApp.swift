88562b35e25029b22bfdb65de4d9cbf3aee334fb
justanobody2107 <andreadallara.2107@gmail.com>
2023-04-14 23:04:30 -0400

    Initial Commit

---
+++ iFap/iFapApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFapApp.swift
+//  iFap
+//
+//  Created by Dre Dall'Ara on 4/14/23.
+//
+
+import SwiftUI
+
+@main
+struct iFapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
