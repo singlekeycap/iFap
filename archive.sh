282d5c4d47a5175322313c979d45300f436ea012
justanobody2107 <andreadallara.2107@gmail.com>
2023-06-24 19:06:21 -0400

    Fix archiver and gitignore

--- archive.sh
+++ archive.sh
@@ -15,7 +15,7 @@ mkdir one.keycap.ifap_${version}_iphoneos-arm64/DEBIAN
 mkdir one.keycap.ifap_${version}_iphoneos-arm64/var
 mkdir one.keycap.ifap_${version}_iphoneos-arm64/var/jb
 mkdir one.keycap.ifap_${version}_iphoneos-arm64/var/jb/Applications
-cp -r build/Build/Products/Release-iphoneos/iFap.app one.keycap.ifap_${version}_iphoneos-arm/var/jb/Applications/
+cp -r build/Build/Products/Release-iphoneos/iFap.app one.keycap.ifap_${version}_iphoneos-arm64/var/jb/Applications/
 echo -e "Package: one.keycap.ifap\nName: iFap\nIcon: https://repo.keycap.one/icons/iFap.png\nDepends: firmware (>= 15.0)\nArchitecture: iphoneos-arm64\nDescription: An all-in-one app that has multiple sources of R-rated material\nMaintainer: SingleKeycap\nAuthor: SingleKeycap\nSection: Applications\nVersion: ${version}\nInstalled-Size: $(du -ks one.keycap.ifap_${version}_iphoneos-arm/var|cut -f 1)\nSileoDepiction: https://repo.keycap.one/depictions/one.keycap.ifap.json\n" > one.keycap.ifap_${version}_iphoneos-arm64/DEBIAN/control
 chmod -R 0755 one.keycap.ifap_${version}_iphoneos-arm64/DEBIAN
 dpkg-deb --build one.keycap.ifap_${version}_iphoneos-arm64
